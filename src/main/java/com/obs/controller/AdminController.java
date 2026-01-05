a31cce5fc3cfc75bd44935a7e8e7d32b8d4e2614
Neha <NEHA-SHARAD.PATIL@CAPGEMINI.COM>
2026-01-05 18:14:13 +0530

    Approve large transactions by admin

--- src/main/java/com/obs/controller/AdminController.java
+++ src/main/java/com/obs/controller/AdminController.java
@@ -1,10 +1,12 @@
 
 package com.obs.controller;
 
+import com.obs.entity.Transaction;
 import com.obs.entity.User;
 import com.obs.payload.request.SignupRequest;
 import com.obs.payload.response.MessageResponse;
 
+import com.obs.service.Interfaces.ITransactionService;
 import com.obs.service.Interfaces.IUserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,6 +22,9 @@ public class AdminController {
 
     @Autowired
     private  IUserService userService;
+
+    @Autowired
+    private ITransactionService transactionService;
 
     @PostMapping("/create-banker")
     @PreAuthorize("hasRole('ADMIN')")
@@ -40,5 +45,26 @@ public class AdminController {
         userService.toggleUserActive(id);
         return ResponseEntity.ok(new MessageResponse("User activation status updated successfully!"));
     }
+
+    @GetMapping("/pending-transactions")
+    @PreAuthorize("hasRole('ADMIN')")
+    public List<Transaction> getPendingTransactions() {
+        return transactionService.getPendingTransactions();
+    }
+
+    @PutMapping("/transactions/{id}/approve")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> approveTransaction(@PathVariable Long id) {
+        transactionService.approveTransaction(id);
+        return ResponseEntity.ok(new MessageResponse("Transaction approved successfully by Admin"));
+    }
+
+    @PutMapping("/transactions/{id}/reject")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<?> rejectTransaction(@PathVariable Long id) {
+        transactionService.rejectTransaction(id);
+        return ResponseEntity.ok(new MessageResponse("Transaction rejected successfully by Admin"));
+    }
+
 }
 
